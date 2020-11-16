2c9ce8331d41a082ec695c4e310f0c3c19ccaaa5
ibahadiraltun <ibahadiraltun@gmail.com>
2020-11-16 23:20:27 +0300

    add team info

--- web-app/main.php
+++ web-app/main.php
@@ -25,6 +25,20 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     </div>
     <p>
         <a href="logout.php" class="btn btn-danger">Logout</a>
+        <div class="page-header">
+        <h2> Sections </h2>
+        </div>
+        <a href="organizations/organization-list.php" class="btn btn-primary">Organizations</a>
+        <a href="products/product-list.php" class="btn btn-primary">Products</a>
+        <a href="manufacturers/manufacturers-list.php" class="btn btn-primary">Manufacturers</a>
+        <a href="features/features-list.php" class="btn btn-primary">Features</a>
+        <a href="product-brands/product-brands-list.php" class="btn btn-primary">Product Brands</a>
+        <div class="page-header">
+        <h2> Team </h2>
+        <div> Bahadir Altun </div>
+        <div> Rabia Yunusoglu </div>
+        <div> Furkan Dolasik </div>
+        </div>
     </p>
 </body>
 </html>
