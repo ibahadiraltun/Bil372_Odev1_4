bab25ae90dc1b3b72caa98ee676b6eb8505b0e9d
ibahadiraltun <ibahadiraltun@gmail.com>
2020-11-16 19:10:46 +0300

    finalize crud for organizations

--- web-app/organizations/save.php
+++ web-app/organizations/save.php
@@ -25,10 +25,12 @@ if(count($_POST)>0){
 	if($_POST['type']==2){
 		$id=$_POST['id'];
 		$name=$_POST['name'];
-		$email=$_POST['email'];
-		$phone=$_POST['phone'];
-		$city=$_POST['city'];
-		$sql = "UPDATE `organizations` SET `name`='$name',`email`='$email',`phone`='$phone',`city`='$city' WHERE id=$id";
+		$abstract=$_POST['abstract'];
+		$address=$_POST['address'];
+    $city=$_POST['city'];
+    $district=$_POST['district'];
+		$type=$_POST['_type'];
+		$sql = "UPDATE `organizations` SET `org_name`='$name',`org_abstract`='$abstract',`org_address`='$address',`org_city`='$city', `org_district`='$district',`org_type`='$type' WHERE id=$id";
 		if (mysqli_query($link, $sql)) {
 			echo json_encode(array("statusCode"=>200));
 		} 
@@ -41,7 +43,7 @@ if(count($_POST)>0){
 if(count($_POST)>0){
 	if($_POST['type']==3){
 		$id=$_POST['id'];
-		$sql = "DELETE FROM `organizations` WHERE id=$id ";
+		$sql = "DELETE FROM `organizations` WHERE org_id=$id ";
 		if (mysqli_query($link, $sql)) {
 			echo $id;
 		} 
@@ -54,7 +56,7 @@ if(count($_POST)>0){
 if(count($_POST)>0){
 	if($_POST['type']==4){
 		$id=$_POST['id'];
-		$sql = "DELETE FROM organizations WHERE id in ($id)";
+		$sql = "DELETE FROM organizations WHERE org_id in ($id)";
 		if (mysqli_query($link, $sql)) {
 			echo $id;
 		} 
